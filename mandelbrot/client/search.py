9d164bbca6937bcdcd86bb784ddd903002f15d59
Michael Frank <msfrank@syntaxjockey.com>
2014-06-09 15:20:37 -0700

    add search notifications client command
    * fixed some incorrect configuration settings references caused by
      copy-paste errors

--- mandelbrot/client/search.py
+++ mandelbrot/client/search.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Mandelbrot.  If not, see <http://www.gnu.org/licenses/>.
 
-import calendar
 from urllib import urlencode
 from urlparse import urlparse, urljoin
 from twisted.web.http_headers import Headers
@@ -43,7 +42,7 @@ def search_status_callback(ns):
     # client settings
     section = ns.get_section('client')
     search = section.get_str('supervisor url', ns.get_section('supervisor').get_str('supervisor url'))
-    # client:system:history settings
+    # client:search:history settings
     section = ns.get_section('client:search:status')
     fields = ('probeRef','lifecycle','health','acknowledged','summary','lastChange','lastUpdate','squelched')
     fields = section.get_list('status fields', fields)
@@ -74,12 +73,13 @@ def search_status_callback(ns):
         print "query failed: " + str(e)
         return
 
+
 @action
 def search_history_callback(ns):
     # client settings
     section = ns.get_section('client')
     search = section.get_str('supervisor url', ns.get_section('supervisor').get_str('supervisor url'))
-    # client:system:history settings
+    # client:search:history settings
     section = ns.get_section('client:search:history')
     timerange = section.get_str('history timerange')
     limit = section.get_int('history limit')
@@ -123,6 +123,55 @@ def search_history_callback(ns):
         return
 
 
+@action
+def search_notifications_callback(ns):
+    # client settings
+    section = ns.get_section('client')
+    search = section.get_str('supervisor url', ns.get_section('supervisor').get_str('supervisor url'))
+    # client:search:notifications settings
+    section = ns.get_section('client:search:notifications')
+    timerange = section.get_str('notifications timerange')
+    limit = section.get_int('notifications limit')
+    fields = ('timestamp', 'probeRef', 'kind', 'description', 'correlation')
+    fields = section.get_list('notifications fields', fields)
+    sort = section.get_list('notifications sort', ['timestamp'])
+    tablefmt = section.get_str('notifications table format', 'simple')
+    params = [('q', ' '.join(ns.get_args()))]
+    # build query url
+    if timerange is not None:
+        start,end = parse_timerange(timerange)
+        if start is not None:
+            params.append(('from', start.isoformat()))
+        if end is not None:
+            params.append(('to', end.isoformat()))
+    if limit is not None:
+        params.append(('limit', limit))
+    qs = urlencode(params)
+    # get the notifications
+    try:
+        url = urljoin(search, 'services/notifications/search?' + qs)
+        logger.debug("connecting to %s", url)
+        response = yield http.agent(timeout=3).request('GET', url)
+        logger.debug("received response %i %s", response.code, response.phrase)
+    except Exception, e:
+        print "query failed: " + str(e)
+        return
+    # parse the response body
+    try:
+        body = yield http.read_body(response)
+        logger.debug("received body %s", body)
+        if response.code == 200:
+            results = from_json(body)
+            if len(results) > 0:
+                notifications = sort_results(results, sort)
+                print render_table(notifications, expand=False, columns=fields, renderers=renderers, tablefmt=tablefmt)
+        else:
+            print "server returned error: " + from_json(body)['description']
+    except Exception, e:
+        print "query failed: " + str(e)
+        return
+
+
 from pesky.settings.action import Action, NOACTION
 from pesky.settings.option import *
 
@@ -135,9 +184,9 @@ search_actions = Action("search",
                        usage="[OPTIONS] QUERY",
                        description="retrieve status of all probes matching QUERY",
                        options=[
-                         Option('f', 'fields', 'systems fields', help="display only the specified FIELDS", metavar="FIELDS"),
-                         Option('s', 'sort', 'systems sort', help="sort results using the specified FIELDS", metavar="FIELDS"),
-                         Option('T', 'table-format', 'systems table format', help="display result using the specified FMT", metavar="FMT")
+                         Option('f', 'fields', 'status fields', help="display only the specified FIELDS", metavar="FIELDS"),
+                         Option('s', 'sort', 'status sort', help="sort results using the specified FIELDS", metavar="FIELDS"),
+                         Option('T', 'table-format', 'status table format', help="display result using the specified FMT", metavar="FMT")
                          ],
                        callback=search_status_callback),
                      Action("history",
@@ -146,10 +195,21 @@ search_actions = Action("search",
                        options=[
                          Option('t', 'range', 'history timerange', help="retrieve history within the specified TIMERANGE", metavar="TIMERANGE"),
                          Option('l', 'limit', 'history limit', help="return a maximum of LIMIT results", metavar="LIMIT"),
-                         Option('f', 'fields', 'systems fields', help="display only the specified FIELDS", metavar="FIELDS"),
-                         Option('s', 'sort', 'systems sort', help="sort results using the specified FIELDS", metavar="FIELDS"),
-                         Option('T', 'table-format', 'systems table format', help="display result using the specified FMT", metavar="FMT")
+                         Option('f', 'fields', 'history fields', help="display only the specified FIELDS", metavar="FIELDS"),
+                         Option('s', 'sort', 'history sort', help="sort results using the specified FIELDS", metavar="FIELDS"),
+                         Option('T', 'table-format', 'history table format', help="display result using the specified FMT", metavar="FMT")
                          ],
                        callback=search_history_callback),
+                     Action("notifications",
+                       usage="[OPTIONS] QUERY",
+                       description="retrieve notification history of all probes matching QUERY",
+                       options=[
+                         Option('t', 'range', 'notifications timerange', help="retrieve history within the specified TIMERANGE", metavar="TIMERANGE"),
+                         Option('l', 'limit', 'notifications limit', help="return a maximum of LIMIT results", metavar="LIMIT"),
+                         Option('f', 'fields', 'notifications fields', help="display only the specified FIELDS", metavar="FIELDS"),
+                         Option('s', 'sort', 'notifications sort', help="sort results using the specified FIELDS", metavar="FIELDS"),
+                         Option('T', 'table-format', 'notifications table format', help="display result using the specified FMT", metavar="FMT")
+                         ],
+                       callback=search_notifications_callback),
                      ]
                  )
--- mandelbrot/client/system.py
+++ mandelbrot/client/system.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Mandelbrot.  If not, see <http://www.gnu.org/licenses/>.
 
-import collections, calendar
 from urllib import urlencode
 from urlparse import urlparse, urljoin
 from twisted.internet import reactor
@@ -99,9 +98,9 @@ def system_history_callback(ns):
     if timerange is not None:
         start,end = parse_timerange(timerange)
         if start is not None:
-            params.append(('from', calendar.timegm(start.utctimetuple())))
+            params.append(('from', start.isoformat()))
         if end is not None:
-            params.append(('to', calendar.timegm(end.utctimetuple())))
+            params.append(('to', end.isoformat()))
     if limit is not None:
         params.append(('limit', limit))
     qs = urlencode(params)
@@ -151,9 +150,9 @@ def system_notifications_callback(ns):
     if timerange is not None:
         start,end = parse_timerange(timerange)
         if start is not None:
-            params.append(('from', calendar.timegm(start.utctimetuple())))
+            params.append(('from', start.isoformat()))
         if end is not None:
-            params.append(('to', calendar.timegm(end.utctimetuple())))
+            params.append(('to', end.isoformat()))
     if limit is not None:
         params.append(('limit', limit))
     qs = urlencode(params)
@@ -261,7 +260,7 @@ system_actions = Action("system",
                          Option('l', 'limit', 'notifications limit', help="return a maximum of LIMIT results", metavar="LIMIT"),
                          Option('f', 'fields', 'notifications fields', help="display only the specified FIELDS", metavar="FIELDS"),
                          Option('s', 'sort', 'notifications sort', help="sort results using the specified FIELDS", metavar="FIELDS"),
-                         Option('T', 'table-format', 'table format', help="display result using the specified FMT", metavar="FMT")
+                         Option('T', 'table-format', 'notifications table format', help="display result using the specified FMT", metavar="FMT")
                        ],
                        callback=system_notifications_callback),
                      Action("acknowledge",
