1d688a9a6b7077bb50cc87f424a362673e662852
Michael Frank <msfrank@syntaxjockey.com>
2014-08-19 11:18:19 -0700

    bump dev version to 0.0.8

--- mandelbrot/version.py
+++ mandelbrot/version.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Mandelbrot.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = (0, 0, 7)
+__version__ = (0, 0, 8)
 
 def versionstring():
     """
