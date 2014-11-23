cd627f40d26427aa6473ab95fc3e35775dc35518
Mike Fort <mikefort83@gmail.com>
2014-11-23 13:05:35 -0800

    Changing requirements to list comprehension

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ exec(open(join(topdir, "mandelbrot/version.py"), "r").read())
 readme = open("README.rst", "r").read()
 
 #load requirement contents from requirements.txt
-requirements = filter(None, open("requirements.txt", "r").read().split('\n'))
+requirements = [requirement for requirement in open("requirements.txt", "r").read().split('\n') if requirement]
+print requirements
+exit(0)
 
 setup(
     # package description
