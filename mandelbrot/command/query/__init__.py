8637937c0ec43e9a7b5ea0f0faad5c2ffd889c0d
Michael Frank <msfrank@syntaxjockey.com>
2015-05-16 13:31:03 -0700

    add query notifications command

--- mandelbrot/command/query/__init__.py
+++ mandelbrot/command/query/__init__.py
@@ -7,12 +7,15 @@ def main():
     """
     try:
         import mandelbrot.command.query.condition as condition
+        import mandelbrot.command.query.notifications as notifications
+        import mandelbrot.command.query.metrics as metrics
 
         # create the top-level parser
         parser = argparse.ArgumentParser(prog='mandelbrot-query')
         parser.set_defaults(main=lambda ns: 0)
         subparsers = parser.add_subparsers()
 
+        # query condition
         query_condition = subparsers.add_parser('condition')
         query_condition.set_defaults(main=condition.run_command)
         query_condition.add_argument('-i', '--agent-id', metavar='AGENT', dest='agent_id',
@@ -32,6 +35,48 @@ def main():
                                     choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'], default='INFO',
                                     help='Log at the specified level')
         query_condition.add_argument('check_id', metavar='CHECK')
+
+        # query notifications
+        query_notifications = subparsers.add_parser('notifications')
+        query_notifications.set_defaults(main=notifications.run_command)
+        query_notifications.add_argument('-i', '--agent-id', metavar='AGENT', dest='agent_id',
+                                     help='Query the specified AGENT')
+        query_notifications.add_argument('-t', '--timerange', metavar='TIMERANGE', dest='timerange', default=None,
+                                     help='Request data within the specified TIMERANGE')
+        query_notifications.add_argument('-l', '--limit', metavar='LIMIT', dest='limit', type=int, default=100,
+                                     help='Return at most LIMIT results')
+        query_notifications.add_argument('-r', '--reverse', dest='reverse', action='store_true',
+                                     help='Return results in descending order')
+        query_notifications.add_argument('-u', '--endpoint-url', metavar='URL', dest='endpoint_url',
+                                     help="Query the specified endpoint URL")
+        query_notifications.add_argument('-v', '--verbose', action='store_true')
+        query_notifications.add_argument('-p', '--pool-workers', metavar='NUM', dest='pool_workers',
+                                    type=int, default=10, help='Size of the query worker pool')
+        query_notifications.add_argument('--log-level', metavar='LEVEL', dest='log_level',
+                                    choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'], default='INFO',
+                                    help='Log at the specified level')
+        query_notifications.add_argument('check_id', metavar='CHECK')
+
+        # query metrics
+        query_metrics = subparsers.add_parser('metrics')
+        query_metrics.set_defaults(main=metrics.run_command)
+        query_metrics.add_argument('-i', '--agent-id', metavar='AGENT', dest='agent_id',
+                                         help='Query the specified AGENT')
+        query_metrics.add_argument('-t', '--timerange', metavar='TIMERANGE', dest='timerange', default=None,
+                                         help='Request data within the specified TIMERANGE')
+        query_metrics.add_argument('-l', '--limit', metavar='LIMIT', dest='limit', type=int, default=100,
+                                         help='Return at most LIMIT results')
+        query_metrics.add_argument('-r', '--reverse', dest='reverse', action='store_true',
+                                         help='Return results in descending order')
+        query_metrics.add_argument('-u', '--endpoint-url', metavar='URL', dest='endpoint_url',
+                                         help="Query the specified endpoint URL")
+        query_metrics.add_argument('-v', '--verbose', action='store_true')
+        query_metrics.add_argument('-p', '--pool-workers', metavar='NUM', dest='pool_workers',
+                                         type=int, default=10, help='Size of the query worker pool')
+        query_metrics.add_argument('--log-level', metavar='LEVEL', dest='log_level',
+                                         choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'], default='INFO',
+                                         help='Log at the specified level')
+        query_metrics.add_argument('check_id', metavar='CHECK')
 
         ns = parser.parse_args()
         return ns.main(ns)
---
+++ mandelbrot/command/query/notifications.py
@@ -0,0 +1,85 @@
+import asyncio
+import urllib.parse
+import cifparser
+import logging
+
+from mandelbrot.registry import Registry
+from mandelbrot.query.endpoint import make_endpoint
+from mandelbrot.timerange import parse_timerange
+from mandelbrot.table import Table, Column, Rowstore, Terminal
+from mandelbrot.log import utility_format
+
+def run_command(ns):
+    """
+    """
+    if ns.verbose == True:
+        logging.basicConfig(level=logging.DEBUG, format=utility_format)
+    else:
+        logging.basicConfig(level=logging.WARNING, format=utility_format)
+    log = logging.getLogger('mandelbrot')
+
+    event_loop = asyncio.get_event_loop()
+    registry = Registry()
+
+    agent_id = cifparser.make_path(ns.agent_id)
+    check_id = cifparser.make_path(ns.check_id)
+    if ns.timerange:
+        timerange = parse_timerange(ns.timerange)
+    else:
+        timerange = None
+    limit = ns.limit
+    reverse = ns.reverse
+    endpoint_url = urllib.parse.urlparse(ns.endpoint_url)
+
+    # construct the endpoint
+    log.debug("constructing endpoint %s", endpoint_url)
+    with make_endpoint(event_loop, endpoint_url, registry, 10) as endpoint:
+
+        # query the endpoint, store the results in rowstore
+        rowstore = Rowstore()
+        if timerange:
+            nleft = limit
+            last = None
+            while nleft > 0:
+                coro = endpoint.list_notifications(agent_id, check_id, nleft,
+                                                   start=timerange[0], end=timerange[1], last=last, descending=reverse)
+                page = event_loop.run_until_complete(coro)
+                for check_notifications in page.list_check_notifications():
+                    for notification in check_notifications.list_notifications():
+                        rowstore.append_row(notification_to_row(notification))
+                    nleft -= 1
+                last = page.get_last()
+                if page.get_exhausted():
+                    break
+        else:
+            coro = endpoint.get_current_notifications(agent_id, check_id)
+            check_notifications = event_loop.run_until_complete(coro)
+            for notification in check_notifications.list_notifications():
+                rowstore.append_row(notification_to_row(notification))
+
+        # render the rowstore in a table
+        table = Table()
+        terminal = Terminal()
+        table.append_column(Column('Timestamp', 'timestamp', normalize=False))
+        table.append_column(Column('Kind', 'kind', normalize=False))
+        table.append_column(Column('Description', 'description', expand=True))
+        table.append_column(Column('Correlation', 'correlation', normalize=False))
+        table.print_table(rowstore, terminal)
+
+    return 0
+
+def notification_to_row(notification):
+    """
+    :param notification:
+    :type notification: Notification
+    :return:
+    """
+    row = {
+        'timestamp': notification.get_timestamp().get_datetime().isoformat(),
+        'kind': notification.get_kind(),
+        'description': notification.get_description(),
+    }
+    correlation = notification.get_correlation()
+    if correlation:
+        row['correlation'] = correlation
+    return row
